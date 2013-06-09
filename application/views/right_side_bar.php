1d77ddf77350233c1db5b676e9622a707fca8134
rinzaimedia <rinzaimedia@gmail.com>
2013-06-08 23:07:53 -0700

    made changes to advertising module

--- application/views/right_side_bar.php
+++ application/views/right_side_bar.php
@@ -1,7 +1,25 @@
 <!-- Side bar -->
 <div id="Side_Bar" class="clsFloatRight">
     <div class="clsSide_Add">
-        <a href="#"><img src="<?php echo base_url()?>images/add.png" alt="image" /></a>
+        <?php
+
+        $banner_query = $this->common_model->getTableData("ad_banner","1");
+
+        $banners=$banner_query->result();
+
+            if($banners != '')
+            {
+                $count = rand(0,count($banners)-1);
+
+
+                        echo $banners[$count]->code;
+
+                        break;
+
+            }
+        ?>
+
+        <!--<a href="#"><img src="<?php echo base_url()?>images/add.png" alt="image" /></a>-->
     </div>
     <div class="clsFace_Likers">
     	<?php
