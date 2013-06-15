3e63e4cb2f87bdd5b5e8c656ec4353a85f363a97
rinzaimedia <rinzaimedia@gmail.com>
2013-06-14 23:36:51 -0700

    made changes to page module

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,7 @@
         </div>
     </div>
     <div id="Footer">
-    	<p><a href="#">Recent Plans</a>|<a href="#">How it works</a>|<a href="<?php echo base_url()."home/faq"?>">FAQ</a>|<a href="#">Promote your business</a>|<a href="#">Press</a></p>
+    	<p><a href="/pages/merchant-faq">Merchants</a>|<a href="#">How it works</a>|<a href="<?php echo base_url()."home/faq"?>">FAQ</a>|<a href="#">Promote your business</a>|<a href="#">Press</a></p>
         <p><a href="#">Contact</a>|<a href="#">Terms of Use</a>|<a href="#">Privacy Policy</a></p>
     </div>
 </div>
